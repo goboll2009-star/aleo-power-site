18b891fcdb370ad6b91ce12a789f7540aa9d7a8f
goboll2009-star <goboll2009@gmail.com>
2026-06-26 20:11:50 +0800

    Add app/layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+import { WhatsAppButton } from "./components";
+
+export const metadata = {
+  title: "WEUP Chinese Herbal Ingredients",
+  description:
+    "B2B supplier of Chinese herbal raw materials, botanical extracts, TCM ingredients and OEM private label herbal products."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <WhatsAppButton />
+      </body>
+    </html>
+  );
+}
